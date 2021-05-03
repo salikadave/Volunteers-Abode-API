67b46891a7936b73c2c0a132e70970f9a81a9d00
Salika Dave <salikadave26@gmail.com>
2021-05-03 18:19:32 +0530

    add error handling in neo4j-calls

--- db-init.js
+++ db-init.js
@@ -1,26 +1,32 @@
-let neo4j = require('neo4j-driver');
+let neo4j = require("neo4j-driver");
 // let { creds } = require("./../config/credentials");
-let driver = neo4j.driver("bolt://0.0.0.0:7687", neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PWD));
+let driver = neo4j.driver(
+  "bolt://0.0.0.0:7687",
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PWD)
+);
 
 exports.get_num_nodes = async function () {
-    let session = driver.session();
-    const num_nodes = await session.run('MATCH (n) RETURN n', {
-    });
-    session.close();
-    console.log("RESULT", (!num_nodes ? 0 : num_nodes.records.length));
-    return (!num_nodes ? 0 : num_nodes.records);
+  let session = driver.session();
+  const num_nodes = await session
+    .run("MATCH (n) RETURN n", {})
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => console.log(err));
+  session.close();
+  console.log("RESULT", !num_nodes ? 0 : num_nodes.records.length);
+  return !num_nodes ? 0 : num_nodes.records;
 };
 exports.create_user = async function (name) {
-    let session = driver.session();
-    let user = "No User Was Created";
-    try {
-        user = await session.run('MERGE (n:user {name: $id}) RETURN n', {
-            id: name
-        });
-    }
-    catch (err) {
-        console.error(err);
-        return user;
-    }
-    return user.records[0].get(0).properties.name;
-}
+  let session = driver.session();
+  let user = "No User Was Created";
+  try {
+    user = await session.run("MERGE (n:user {name: $id}) RETURN n", {
+      id: name,
+    });
+  } catch (err) {
+    console.error(err);
+    return user;
+  }
+  return user.records[0].get(0).properties.name;
+};
