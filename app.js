714372bbeac7f8e66e5b98d13750e84cfcb98375
Salika Dave <salikadave26@gmail.com>
2021-05-16 17:57:21 +0530

    add allowed headers

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// Handling CORS errors on client-side
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    return res.status(200).json({
+      message: "Server is CORS enabled",
+    });
+  }
+  next();
+});
+
 // Bind Neo4j in every request
 app.use((req, res, next) => {
   req.neo4j = require("./neo4j");
