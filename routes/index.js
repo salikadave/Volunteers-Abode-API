6a951a7b251a41f407307e44a719cc227d9e26f6
Salika Dave <salikadave26@gmail.com>
2021-05-17 23:21:26 +0530

    add skeleton for requests routes

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ router.use("/volunteer", require("./volunteer"));
 router.use("/ngoadmin", require("./ngoadmin"));
 router.use("/posts", require("./post"));
 router.use("/events", require("./events"));
+router.use("/resource", require("./resources"));
 
 module.exports = router;
---
+++ routes/resources/index.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+// Open a Resource Request
+// router.use("/request", require("./request"));
+// Resolve a Resource Request
+// router.use("/resolve", require("./resolution"));
+// Update request details
+// router.use("/update", require("./update"));
+// Delete request
+// router.use("/delete", require("./delete"));
+
+module.exports = router;
