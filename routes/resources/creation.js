5630b852900abd4762bc0d6d251bf6029c72ff35
Salika Dave <salikadave26@gmail.com>
2021-05-19 01:24:27 +0530

    add request creation route

---
+++ routes/resources/creation.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const query = require("../../cypher");
+const checkAuth = require("../../middleware/checkAuth");
+
+// Create Request
+router.post("/", checkAuth, (req, res) => {
+  console.log("inside request");
+  const params = {
+    creatorID: req.body.reqBy,
+    catArr: req.body.categories,
+    reqTitle: req.body.title,
+    reqDetails: req.body.details,
+    amtArr: req.body.amtRequired,
+    count: req.body.volCount,
+    coordsArr: req.body.mapCoords,
+    img: req.body.imageURL,
+  };
+  if (
+    !params.img ||
+    !params.creatorID ||
+    !params.reqTitle ||
+    !params.amtArr ||
+    !params.catArr
+  ) {
+    return res.status(422).json({ error: "Please add all the fields" });
+  } else {
+    req.neo4j
+      .write(query("create-request-volunteer"), params)
+      .then((result) => {
+        let fetched = {
+          reqDetails: result.records[0].get("rr"),
+          timestamp: result.records[0].get("timestamp").toNumber(),
+        };
+        return fetched;
+      })
+      .then((data) => {
+        res.status(200).send({
+          message: "Request added successfully!",
+          details: { ...data.reqDetails.properties, timestamp: data.timestamp },
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+});
+
+module.exports = router;
--- routes/resources/index.js
+++ routes/resources/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 // Get All Requests
 router.use("/", require("./details"));
 // Open a Resource Request
-// router.use("/create", require("./creation"));
+router.use("/create", require("./creation"));
 // Resolve a Resource Request
 // router.use("/resolve", require("./resolution"));
 // Update request details
